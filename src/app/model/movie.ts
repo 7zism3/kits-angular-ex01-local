7b8d38e3ecd3da8831abaac6cef813fec8e2d2ba
7zism3 <7zism3@gmail.com>
2021-07-14 00:09:09 +0700

    14/07

--- src/app/model/movie.ts
+++ src/app/model/movie.ts
@@ -1,6 +1,10 @@
-export class Movie2 {
+export class Movie {
+  
+  id: number = 1;
   title: string = '';
   director: string = '';
   cast: string = '';
   releaseDate: Date = new Date();
+
+  constructor() {}
 }
--- src/app/ngfor-ex/ngfor-ex.component.ts
+++ src/app/ngfor-ex/ngfor-ex.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Movie2 } from '../model/movie';
 
 @Component({
   selector: 'app-ngfor-ex',
@@ -7,7 +6,7 @@ import { Movie2 } from '../model/movie';
   styleUrls: ['./ngfor-ex.component.css'],
 })
 export class NgforExComponent implements OnInit {
-  movieAdd: Movie2 = new Movie2();
+  movieAdd: Movie = new Movie();
 
   constructor() {}
 
@@ -16,7 +15,7 @@ export class NgforExComponent implements OnInit {
   title: string = 'Top 5 Movies';
 
   get itemNum() {
-    return this.movies2.length;
+    return this.movies.length;
   }
 
   movies: Movie[] = [
@@ -52,50 +51,17 @@ export class NgforExComponent implements OnInit {
     },
   ];
 
-  movies2: Movie2[] = [
-    {
-      title: 'Zootopia',
-      director: 'Byron Howard, Rich Moore',
-      cast: 'Idris Elba, Ginnifer Goodwin, Jason Bateman',
-      releaseDate: new Date(2016, 3, 4),
-    },
-    {
-      title: 'Batman v Superman: Dawn of Justice',
-      director: 'Zack Snyder',
-      cast: 'Ben Affleck, Henry Cavill, Amy Adams',
-      releaseDate: new Date(2016, 3, 25),
-    },
-    {
-      title: 'Captain American: Civil War',
-      director: 'Anthony Russo, Joe Russo',
-      cast: 'Scarlett Johansson, Elizabeth Olsen, Chris Evans',
-      releaseDate: new Date(2016, 5, 6),
-    },
-    {
-      title: 'X-Men: Apocalypse',
-      director: 'Bryan Singer',
-      cast: 'Jennifer Lawrence, Olivia Munn, Oscar Isaac',
-      releaseDate: new Date(2016, 5, 27),
-    },
-    {
-      title: 'Warcraft',
-      director: 'Duncan Jones',
-      cast: 'Travis Fimmel, Robert Kazinsky, Ben Foster',
-      releaseDate: new Date(2016, 6, 10),
-    },
-  ];
-
-  CompositeKey(index: any, item: Movie2) {
+  CompositeKey(index: any, item: Movie) {
     return item.title + item.director;
   }
 
   deleteMovie(index: number): void {
-    this.movies2.splice(index, 1);
+    this.movies.splice(index, 1);
     alert('Bạn có muốn xoá?');
   }
   addMovie(): void {
-    this.movies2.push(this.movieAdd);
-    this.movieAdd = new Movie2();
+    this.movies.push(this.movieAdd);
+    this.movieAdd = new Movie();
   }
 }
 
